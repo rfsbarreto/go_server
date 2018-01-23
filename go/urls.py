31a6b701d49b85046947d2781018e8e4cd411f6d
Raphael Barreto <rfsbarreto@gmail.com>
2018-01-23 17:14:27 -0300

    Renamed App

--- go/urls.py
+++ go/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 
 from django.contrib import admin
+from welcome.views import health
 admin.autodiscover()
 
 import web.views
@@ -14,6 +15,7 @@ import gotrack.views
 
 urlpatterns = [
     url(r'^$', web.views.index, name='index'),
+    url(r'^health$', health),
     url(r'^go_bus/record/', gobus.views.go_bus_record, name='go_bus_record'),
     url(r'^go_track/record/', gotrack.views.go_track_record, name='go_track_record'),
     url(r'^welcome/', include('welcome.urls')),
